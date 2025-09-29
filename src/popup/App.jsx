58d27cd0ddb1f819dcda8569f85827f1b93534ee
thatonedeveloperguy <Harikrishna3@>
2025-09-29 19:26:50 +0530

    conditional-oboarding

--- src/popup/App.jsx
+++ src/popup/App.jsx
@@ -1,13 +1,19 @@
-import { Routes, Route, useNavigate } from "react-router-dom"
+import { Routes, Route } from "react-router-dom"
 import Login from "./Login"
 import Quiz from "./Quiz"
 import Recap from "./Recap"
 import Onboarding from "./Onboarding"
+import withOnboardingCheck from "./HOC"
+
+// Wrap the Quiz page with onboarding check
+const QuizWithCheck = withOnboardingCheck(Quiz)
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<Onboarding />} />
+      {/* "/" will show Quiz if completed, otherwise Onboarding */}
+      <Route path="/" element={<QuizWithCheck />} />
+
       <Route path="/login" element={<Login />} />
       <Route path="/quiz" element={<Quiz />} />
       <Route path="/recap" element={<Recap />} />

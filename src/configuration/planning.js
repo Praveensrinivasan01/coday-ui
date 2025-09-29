b0e174c242b5b129d28cce85b164951240aa752c
Praveen <praveensrinivasan01@gmail.com>
2025-09-29 23:45:27 +0530

    planning.js

---
+++ src/configuration/planning.js
@@ -0,0 +1,12 @@
+// Tasks yet to be completed
+
+//U can try again with new qiestion when u login -> login button -> email enter -> passkey -> |  db user -> free tier (basic) -> 1retry kudukanum
+
+//U can try again by upgrading the plan -> see pricing and plan -> future(socket.io)
+// small settings button-> only loginned users-> logout button -> settings -> configuration 
+// -> passkey -> enter this -> 
+//db intha user login la than irukanaaa -> login access token plus-> still login la irukanaa ->
+// one column, premium passkey allowance -> 1 -> only one member can able to use that passkey
+
+
+//U have reached ur maximum retries -come back tomorrow

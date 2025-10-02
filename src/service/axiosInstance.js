dffd891f9d5bc700bf8957613c5c70932dad7a1e
thatonedeveloperguy <Harikrishna3@>
2025-10-02 17:00:38 +0530

    cors fix

--- src/service/axiosInstance.js
+++ src/service/axiosInstance.js
@@ -2,9 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL: "http://localhost:3000", // your backend
-  headers: {
-    "Content-Type": "application/json",
-  },
+headers: { "Content-Type": "application/json" },
   timeout: 10000,
   withCredentials: true, // if you use cookie-based auth
 });
